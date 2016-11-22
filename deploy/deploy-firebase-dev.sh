e807bf3e59a2cbd02a82c691d89f9a16a8ce0d34
Patrice Chalin <chalin@users.noreply.github.com>
2016-11-21 17:34:27 -0800

    chore(travis): use main TOKEN to deploy to dev fb proj (#147)

--- deploy/deploy-firebase-dev.sh
+++ deploy/deploy-firebase-dev.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 echo "================ Deploy to Firebase (dev) ========================"
-firebase deploy --token "${FIREBASE_TOKEN_DEV}" --project dev
+firebase deploy --token "${FIREBASE_TOKEN}" --project dev
