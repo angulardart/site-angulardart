104b47ca58914f9e27461abdcc2a240070a25c76
Patrice Chalin <pchalin@gmail.com>
2016-12-19 13:43:51 -0800

    docs(api): use git repo version of dartdoc, and setup frags
    - use git repo version of dartdoc until most recent changes make it
    into the next SDK release
    - `gulp/dartdoc.js`: add step to setup access to doc sample fragments
    - misc adjustments to `scripts/config/linkcheck-skip-list.txt`
    following recent fixes to the linkcheck tool

--- gulp/dartdoc.js
+++ gulp/dartdoc.js
@@ -3,16 +3,22 @@
 module.exports = function(gulp, plugins, config) {
 
   const argv = plugins.argv;
+  const cp = plugins.child_process;
   const fs = plugins.fs;
   const path = plugins.path;
 
-  gulp.task('dartdoc', () => {
+  const dartdocCmd = 'pub global run dartdoc'
+
+  gulp.task('dartdoc', ['_setup-fragments-for-dartdoc'], () => {
     const ngRepoPath = config.angularRepo;
     if (argv.fast && fs.existsSync(path.resolve(ngRepoPath, config.relDartDocApiDir))) {
       plugins.gutil.log(`Skipping dartdoc: --fast flag enabled and api dir exists (${config.relDartDocApiDir})`);
       return true;
     }
-    return plugins.execp(`dartdoc --output ${config.relDartDocApiDir}`, { cwd: config.angularRepo });
+    return plugins.q.all(
+      plugins.execp(`${dartdocCmd} --version`),
+      plugins.execp(`${dartdocCmd} --output ${config.relDartDocApiDir}`, { cwd: config.angularRepo })
+    );
     // // checkAngularProjectPath(ngRepoPath);
     // const topLevelLibFilePath = path.resolve(ngRepoPath, 'lib', 'angular2.dart');
     // const tmpPath = topLevelLibFilePath + '.disabled';
@@ -24,6 +30,25 @@ module.exports = function(gulp, plugins, config) {
     //     gutil.log(`Restoring top-level angular2 library: ${topLevelLibFilePath}`);
     //     renameIfExistsSync(tmpPath, topLevelLibFilePath);
     // })
+  });
+
+  gulp.task('_setup-fragments-for-dartdoc', () => {
+    const fragmentsDir = 'src/angular/_fragments';
+    const ngRepoPath = config.angularRepo;
+    const docsDir = path.resolve(ngRepoPath, 'docs');
+
+    cp.execSync(`rm -Rf ${docsDir}`);
+    cp.execSync(`mkdir ${docsDir}`);
+
+    plugins.gutil.log(`Linking to fragment folders; cd ${docsDir}`);
+    const fragFolders = plugins.fs.readdirSync(fragmentsDir);
+    fragFolders.forEach(subdir => {
+      const fragSubdir = path.resolve(fragmentsDir, subdir, 'dart');
+      const cmd = `ln -s ${fragSubdir} ${subdir}`;
+      plugins.gutil.log(cmd);
+      cp.execSync(cmd, {cwd:docsDir});
+    });
+    return true;
   });
 
 };
