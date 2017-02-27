83196b6ff9f9891226603cf7cef1955a15f2f513
Patrice Chalin <chalin@users.noreply.github.com>
2017-02-27 14:31:31 -0800

    [ng] QuickStart updates from stagehand 1.0.22 (#420)
    - Move the call of `./scripts/get-ng-web-simple.sh` from the Travis
    install task to the
    `update-web-simple` gulp task so that we can control the
    (re-)invocation of stagehand when building locally.
    - Update the QuickStart sources based on the stagehand template updates.
    
    Fixes #419

--- gulp/update-web-simple.js
+++ gulp/update-web-simple.js
@@ -2,6 +2,8 @@
 
 module.exports = function (gulp, plugins, config) {
 
+  const cp = plugins.child_process;
+  const fs = plugins.fs;
   const path = plugins.path;
   const replace = plugins.replace;
 
@@ -16,6 +18,8 @@ module.exports = function (gulp, plugins, config) {
 
   gulp.task('update-web-simple', cb => {
     const baseDir = webSimpleProjPath;
+    if (plugins.argv.clean) cp.execSync(`rm -Rf ${baseDir}`);
+    if (!fs.existsSync(baseDir)) cp.execSync(`./scripts/get-ng-web-simple.sh`);
     return gulp.src([
       `${baseDir}/pubspec.yaml`,
       `${baseDir}/web/index.html`,
@@ -24,12 +28,15 @@ module.exports = function (gulp, plugins, config) {
     ], { base: baseDir })
       // pubspec.yaml
       .pipe(replace(/^name: angular_quickstart/, '# #docregion\n$&'))
+      .pipe(replace(/(^description: )A simple AngularDart app/m, '$1QuickStart'))
+      .pipe(replace(/(^  angular2: \^).*/m, '$13.0.0-alpha'))
       // index.html
       .pipe(replace(/^<!DOCTYPE html>/, '<!-- #docregion -->\n$&'))
       .pipe(replace(/(\s*)(<my-app>[^<]*<\/my-app>.*)/,
         '$1<!-- #docregion my-app -->$1$2$1<!-- #enddocregion my-app -->'))
       // *.dart
-      .pipe(replace(/^import 'package:angular.*/, '// #docregion\n$&'))
+      // Add docregion at top of file and remove (bogus) author from copyright notice.
+      .pipe(replace(/^(\/\/ Copyright \(c\) 20\d\d)[^\.]*\./, '// #docregion\n$1.'))
       .pipe(gulp.dest('public/docs/_examples/quickstart/dart'));
   });
 
