d88378d9fa9b09ad9698c8c288872dbfd9093d9c
Patrice Chalin <chalin@users.noreply.github.com>
2017-04-06 22:05:13 -0500

    chore: script message copyedit (#531)

--- scripts/serve_local.sh
+++ scripts/serve_local.sh
@@ -8,6 +8,6 @@ bundle exec jekyll build --incremental --watch &
 j_pid=$!
 firebase serve --port 4001 &
 f_pid=$!
-echo "catched PIDs: $j_pid, $f_pid"
+echo "cached PIDs: $j_pid, $f_pid"
 trap "{ kill $j_pid; kill $f_pid; exit 0;}" SIGINT
 wait
