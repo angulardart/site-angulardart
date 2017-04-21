5a995e39975668b398139783b4b4fb3b213d55a0
Patrice Chalin <chalin@users.noreply.github.com>
2017-04-21 10:40:57 -0400

    [ng] gulp task to update SDK vers & updated example pubspecs (#559)
    * [ng] gulp task to update SDK vers
    
    * updated pubspecs

--- gulp/update-ng-vers.js
+++ gulp/update-ng-vers.js
@@ -1,4 +1,4 @@
-// Gulp tasks related to updating the pubspec version of angular.
+// Gulp tasks related to updating the pubspec version of angular & sdk
 'use strict';
 
 module.exports = function (gulp, plugins, config) {
@@ -6,6 +6,26 @@ module.exports = function (gulp, plugins, config) {
   const EXAMPLES_PATH = config.EXAMPLES_PATH;
   const path = plugins.path;
   const replace = plugins.replace;
+
+  gulp.task('update-pubspec', ['update-ng-vers', 'update-sdk-vers']);
+
+  //---------------------------------------------------------------------------
+  // Updating SDK version
+
+  const SDK_VERS = '>=1.23.0-dev <2.0.0';
+
+  gulp.task('update-sdk-vers', ['update-ng-vers'], (cb) => {
+    const baseDir = path.resolve(EXAMPLES_PATH, '..');
+    return gulp.src([
+      `${baseDir}/**/pubspec.yaml`,
+      `!${baseDir}/**/.pub/**`,
+    ]) // , { base: baseDir }
+      .pipe(replace(/^(\s+sdk): ['"][^'"]+['"]/m, `$1: '${SDK_VERS}'`))
+      .pipe(gulp.dest(baseDir));
+  });
+
+  //---------------------------------------------------------------------------
+  // Updating ACX and NG versions
 
   const ACX_VERS = '^0.4.1-beta';
   const NG_VERS = '^3.0.0-beta+1';
@@ -24,7 +44,7 @@ module.exports = function (gulp, plugins, config) {
     const baseDir = path.resolve(EXAMPLES_PATH, '..');
     return gulp.src([
       `${baseDir}/**/pubspec.yaml`,
-      `!${baseDir}/**/.pub/**/pubspec.yaml`,
+      `!${baseDir}/**/.pub/**`,
     ]) // , { base: baseDir }
       .pipe(replace(/(angular2_components): ['"][^'"]+['"]/, `$1: ${ACX_VERS}`))
       .pipe(replace(/(angular2_components): \^\S+/, `$1: ${ACX_VERS}`))
@@ -35,7 +55,7 @@ module.exports = function (gulp, plugins, config) {
     const baseDir = path.resolve(EXAMPLES_PATH, '..');
     return gulp.src([
       `${baseDir}/**/pubspec.yaml`,
-      `!${baseDir}/**/.pub/**/pubspec.yaml`,
+      `!${baseDir}/**/.pub/**`,
     ]) // , { base: baseDir }
       .pipe(replace(/angular2: ['"][^'"]+['"]/, `angular2: ${NG_VERS}`))
       .pipe(replace(/angular2: \^\S+/, `angular2: ${NG_VERS}`))
@@ -47,7 +67,7 @@ module.exports = function (gulp, plugins, config) {
     return gulp.src([
       `${baseDir}/**/pubspec.yaml`,
       `!${baseDir}/ng_test/github_issues/pubspec.yaml`,
-      `!${baseDir}/**/.pub/**/pubspec.yaml`,
+      `!${baseDir}/**/.pub/**`,
     ]) // , { base: baseDir }
       .pipe(replace(/\btransformers:/, `${depOvr}$&`))
       .pipe(gulp.dest(baseDir));
@@ -58,7 +78,7 @@ module.exports = function (gulp, plugins, config) {
     return gulp.src([
       `${baseDir}/**/pubspec.yaml`,
       `!${baseDir}/ng_test/github_issues/pubspec.yaml`,
-      `!${baseDir}/**/.pub/**/pubspec.yaml`,
+      `!${baseDir}/**/.pub/**`,
     ]) // , { base: baseDir }
       .pipe(replace('git: https://github.com/dart-lang/angular2.git', `${depOvr2}`))
       .pipe(gulp.dest(baseDir));
@@ -69,7 +89,7 @@ module.exports = function (gulp, plugins, config) {
     return gulp.src([
       `${baseDir}/**/pubspec.yaml`,
       `!${baseDir}/ng_test/github_issues/pubspec.yaml`,
-      `!${baseDir}/**/.pub/**/pubspec.yaml`,
+      `!${baseDir}/**/.pub/**`,
     ]) // , { base: baseDir }
       .pipe(replace(depOvr3, ''))
       .pipe(gulp.dest(baseDir));
