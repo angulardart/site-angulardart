cb22c77f414f283bbcff9a228bb873153a65fcb4
Patrice Chalin <chalin@users.noreply.github.com>
2017-05-22 16:55:16 -0400

    [ng] toh-1: fix class name; show what happens when directives are missing (#646)
    * [ng] toh-1: fix class name in prose; show what happens when missing directives
    
    - Fixed error in prose: `Hero` class name was used instead of `AppComponent`
    - Show reader what error s/he will get when ng encounters a directive that it doesn't recognize (the template compiler throws an error).
    - Other misc formatting updates to toh-1 code.
    
    * post-review edits suggested by Kathy

--- examples/ng/doc/toh-1/lib/app_component_1.dart
+++ examples/ng/doc/toh-1/lib/app_component_1.dart
@@ -1,26 +1,27 @@
 bogus({template: String}) {}
 
 var t1 = bogus(
-    // #docregion show-hero
-    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
-    // #enddocregion show-hero
-    );
+  // #docregion show-hero
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>',
+  // #enddocregion show-hero
+);
 
 var t2 = bogus(
-    // #docregion show-hero-2
-    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
-    // #enddocregion show-hero-2
-    );
+  // #docregion show-hero-2
+  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>',
+  // #enddocregion show-hero-2
+);
 
 var t4 = bogus(
-    // #docregion multi-line-strings
-    template: '''
-      <h1>{{title}}</h1>
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div><label>name: </label>{{hero.name}}</div>'''
-    // #enddocregion multi-line-strings
-    );
+  // #docregion multi-line-strings
+  template: '''
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+  ''',
+  // #enddocregion multi-line-strings
+);
 
 /*
 // #docregion name-input
