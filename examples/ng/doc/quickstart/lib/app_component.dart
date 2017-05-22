99eaef84d992957fb7b0b3ec4ab981dc12f5dd8f
Patrice Chalin <chalin@users.noreply.github.com>
2017-05-22 12:34:45 -0400

    quickstart, toh-0: fix app comp template formatting (#645)
    Revert to formatting we had before. Adding the trailing comma ensure that dartfmt will preserve this format. (Adding a trailing comma is a good idea in general :)

--- examples/ng/doc/quickstart/lib/app_component.dart
+++ examples/ng/doc/quickstart/lib/app_component.dart
@@ -6,7 +6,8 @@ import 'package:angular2/angular2.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>')
+  template: '<h1>Hello {{name}}</h1>',
+)
 class AppComponent {
   var name = 'Angular';
 }
