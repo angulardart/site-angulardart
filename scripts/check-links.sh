49d5976222ac27bc9af489fa325b13562e1682a5
Patrice Chalin <chalin@users.noreply.github.com>
2017-06-06 05:51:36 -0400

    Travis: switch to using firebase/superstatic (#699)
    * npm -g superstatic
    
    * keep a copy of check-links that uses firebase
    
    * check-links using superstatic w/o any need for authentication

--- scripts/check-links.sh
+++ scripts/check-links.sh
@@ -10,24 +10,15 @@ export CHECK_EXIT_CODE=0
 if [ ! -e "publish" ]; then
   echo "Site not built, skipping link check."
   exit $CHECK_EXIT_CODE
-elif [ -n "$FIREBASE_TOKEN" ]; then
-  FB_TOKEN_OPTN="--token $FIREBASE_TOKEN"
-elif [ -n "$TRAVIS" ]; then
-  echo "===================================================================="
-  echo "Warning: Travis requires that FIREBASE_TOKEN be set so that firebase"
-  echo "Warning: serve can run. It isn't set, so skipping link check."
-  echo "===================================================================="
-  exit $CHECK_EXIT_CODE
 fi
 
 set -x
-: ${FB_PROJ:=dev}
-firebase serve --port $PORT $FB_TOKEN_OPTN --project $FB_PROJ > /dev/null &
-FBS_PID=$!
+superstatic --port $PORT > /dev/null &
+SERVER_PID=$!
 
 sleep 4
 
-linkcheck :4001 --skip-file ./scripts/config/linkcheck-skip-list.txt \
+linkcheck :$PORT --skip-file ./scripts/config/linkcheck-skip-list.txt \
   | tee $TMP/linkcheck-log.txt
 
 set +x
@@ -36,6 +27,6 @@ if ! grep '^\s*0 errors' $TMP/linkcheck-log.txt | wc -l > /dev/null; then
   CHECK_EXIT_CODE=1
 fi
 
-kill $FBS_PID
+kill $SERVER_PID
 
 exit $CHECK_EXIT_CODE
