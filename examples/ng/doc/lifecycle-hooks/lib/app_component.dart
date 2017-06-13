6c2f4b0099c69d1f1fe13065709306f213ba5841
Patrice Chalin <chalin@users.noreply.github.com>
2017-06-13 19:03:32 -0400

    [ng] src reorg of lifecycle-hooks example, and convert Jade to .md (#732)
    * inline cache file
    
    * .jade cleanup
    
    * conversion from jade to md
    
    * figure img --> img
    
    * code excerpts
    
    * rename .jade -> .md
    
    * src reorg proper
    
    * fix tables, adjust paths to code excerpts
    
    * Tweak prose and add `src/` to import paths in app_comp

--- examples/ng/doc/lifecycle-hooks/lib/app_component.dart
+++ examples/ng/doc/lifecycle-hooks/lib/app_component.dart
@@ -1,13 +1,13 @@
 // #docregion
 import 'package:angular2/angular2.dart';
 
-import 'after_content_component.dart';
-import 'after_view_component.dart';
-import 'counter_component.dart';
-import 'do_check_component.dart';
-import 'on_changes_component.dart';
-import 'peek_a_boo_parent_component.dart';
-import 'spy_component.dart';
+import 'src/after_content_component.dart';
+import 'src/after_view_component.dart';
+import 'src/counter_component.dart';
+import 'src/do_check_component.dart';
+import 'src/on_changes_component.dart';
+import 'src/peek_a_boo_parent_component.dart';
+import 'src/spy_component.dart';
 
 @Component(
     selector: 'my-app',
