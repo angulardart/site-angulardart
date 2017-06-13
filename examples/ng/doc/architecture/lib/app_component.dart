db2b8210b37cb504a408f6b622ed187d7b6651e5
Patrice Chalin <chalin@users.noreply.github.com>
2017-06-13 16:57:10 -0400

    [ng] src reorg of architecture example (#726)
    * [ng] src reog of architecture example
    
    Contributes to #713
    
    * post-review edits

--- examples/ng/doc/architecture/lib/app_component.dart
+++ examples/ng/doc/architecture/lib/app_component.dart
@@ -2,14 +2,23 @@
 import 'package:angular2/angular2.dart';
 // #enddocregion import
 
-import 'hero_list_component.dart';
-import 'sales_tax_component.dart';
+import 'src/backend_service.dart';
+import 'src/hero_list_component.dart';
+import 'src/hero_service.dart';
+import 'src/logger_service.dart';
+import 'src/sales_tax_component.dart';
 
+// #docregion providers
 @Component(
-    selector: 'my-app',
-    template: '''
-      <hero-list></hero-list>
-      <sales-tax></sales-tax>''',
-    directives: const [HeroListComponent, SalesTaxComponent])
-// #docregion export
+  // #enddocregion providers
+  selector: 'my-app',
+  template: '''
+    <hero-list></hero-list>
+    <sales-tax></sales-tax>
+  ''',
+  directives: const [HeroListComponent, SalesTaxComponent],
+  // #docregion providers
+  providers: const [BackendService, HeroService, Logger],
+)
+// #docregion class
 class AppComponent {}
