368127ff7dda8753750c85cc7db2c4b81ffc6440
Patrice Chalin <chalin@users.noreply.github.com>
2017-06-14 13:10:04 -0400

    [ng] src reorg of hierarchical-di example, and convert Jade to .md (#731)
    * src reorg proper
    
    * inline TS cache version
    
    * trim whitespace
    
    * conversion from jade to md + some manual tweaks
    
    * renamed to .md
    
    * Don't hard code image width
    
    * manual tweaks: fix !{...} for missing definitions
    
    * add code excerpts
    
    * tweak img width
    
    * Fix code-excerpt paths

--- examples/ng/doc/hierarchical-dependency-injection/lib/app_component.dart
+++ examples/ng/doc/hierarchical-dependency-injection/lib/app_component.dart
@@ -1,34 +1,32 @@
 // #docregion
 import 'package:angular2/angular2.dart';
 
-import 'car_components.dart';
-import 'heroes_list_component.dart';
-import 'heroes_service.dart';
-import 'villains_list_component.dart';
+import 'src/car_components.dart';
+import 'src/heroes_list_component.dart';
+import 'src/heroes_service.dart';
+import 'src/villains_list_component.dart';
 
 @Component(
-    selector: 'my-app',
-    template: '''
-      <label><input type="checkbox" [checked]="showHeroes"   (change)="showHeroes=!showHeroes">Heroes</label>
-      <label><input type="checkbox" [checked]="showVillains" (change)="showVillains=!showVillains">Villains</label>
-      <label><input type="checkbox" [checked]="showCars"     (change)="showCars=!showCars">Cars</label>
+  selector: 'my-app',
+  template: '''
+    <label><input type="checkbox" [checked]="showHeroes"   (change)="showHeroes=!showHeroes">Heroes</label>
+    <label><input type="checkbox" [checked]="showVillains" (change)="showVillains=!showVillains">Villains</label>
+    <label><input type="checkbox" [checked]="showCars"     (change)="showCars=!showCars">Cars</label>
 
-      <h1>Hierarchical Dependency Injection</h1>
+    <h1>Hierarchical Dependency Injection</h1>
 
-      <heroes-list   *ngIf="showHeroes"></heroes-list>
-      <villains-list *ngIf="showVillains"></villains-list>
-      <my-cars       *ngIf="showCars"></my-cars>
-    ''',
-    directives: const [
-      CORE_DIRECTIVES,
-      carComponents,
-      HeroesListComponent,
-      VillainsListComponent
-    ],
-    providers: const [
-      carServices,
-      HeroesService
-    ])
+    <heroes-list   *ngIf="showHeroes"></heroes-list>
+    <villains-list *ngIf="showVillains"></villains-list>
+    <my-cars       *ngIf="showCars"></my-cars>
+  ''',
+  directives: const [
+    CORE_DIRECTIVES,
+    carComponents,
+    HeroesListComponent,
+    VillainsListComponent
+  ],
+  providers: const [carServices, HeroesService],
+)
 class AppComponent {
   bool showCars = true;
   bool showHeroes = true;
