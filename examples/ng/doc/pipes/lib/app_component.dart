191fed7d58772f187a5359831a7afb9e2baab46a
Patrice Chalin <chalin@users.noreply.github.com>
2017-06-13 20:36:01 -0400

    [ng] src reorg of pipes example, and convert Jade to .md (#734)
    * fix whitespace and canonicalize `+makeEx...` mixins
    
    * src reorg proper
    
    * jade to md
    
    * file ext rename .jade to .md
    
    * add code excerpts
    
    * Translate Jade `#{...}` interpolation
    
    * manual adjustments

--- examples/ng/doc/pipes/lib/app_component.dart
+++ examples/ng/doc/pipes/lib/app_component.dart
@@ -1,28 +1,29 @@
 // #docregion
 import 'package:angular2/angular2.dart';
 
-import 'flying_heroes_component.dart';
-import 'hero_async_message_component.dart';
-import 'hero_birthday1_component.dart';
-import 'hero_birthday2_component.dart';
-import 'hero_list_component.dart';
-import 'power_boost_calculator_component.dart';
-import 'power_booster_component.dart';
+import 'src/flying_heroes_component.dart';
+import 'src/hero_async_message_component.dart';
+import 'src/hero_birthday1_component.dart';
+import 'src/hero_birthday2_component.dart';
+import 'src/hero_list_component.dart';
+import 'src/power_boost_calculator_component.dart';
+import 'src/power_booster_component.dart';
 
 @Component(
-    selector: 'my-app',
-    templateUrl: 'app_component.html',
-    directives: const [
-      FlyingHeroesComponent,
-      FlyingHeroesImpureComponent,
-      HeroAsyncMessageComponent,
-      HeroBirthdayComponent,
-      HeroBirthday2Component,
-      HeroListComponent,
-      PowerBoostCalculatorComponent,
-      PowerBoosterComponent,
-    ],
-    pipes: const [COMMON_PIPES])
+  selector: 'my-app',
+  templateUrl: 'app_component.html',
+  directives: const [
+    FlyingHeroesComponent,
+    FlyingHeroesImpureComponent,
+    HeroAsyncMessageComponent,
+    HeroBirthdayComponent,
+    HeroBirthday2Component,
+    HeroListComponent,
+    PowerBoostCalculatorComponent,
+    PowerBoosterComponent,
+  ],
+  pipes: const [COMMON_PIPES],
+)
 class AppComponent {
   DateTime birthday = new DateTime(1988, 4, 15); // April 15, 1988
 }
