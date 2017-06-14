08e8f2e3ab9d1e089a059eca537db671ea35ecc0
Patrice Chalin <chalin@users.noreply.github.com>
2017-06-14 13:10:51 -0400

    [ng] src reorg of security example (#741)
    * move files to src
    
    * Update paths in the prose

--- examples/ng/doc/security/lib/app_component.dart
+++ examples/ng/doc/security/lib/app_component.dart
@@ -1,18 +1,19 @@
 // #docregion
 import 'package:angular2/angular2.dart';
 
-import 'bypass_security_component.dart';
-import 'inner_html_binding_component.dart';
+import 'src/bypass_security_component.dart';
+import 'src/inner_html_binding_component.dart';
 
 @Component(
-    selector: 'my-app',
-    template: '''
-      <h1>Security</h1>
-      <inner-html-binding></inner-html-binding>
-      <bypass-security></bypass-security>
-    ''',
-    directives: const [
-      BypassSecurityComponent,
-      InnerHtmlBindingComponent,
-    ])
+  selector: 'my-app',
+  template: '''
+    <h1>Security</h1>
+    <inner-html-binding></inner-html-binding>
+    <bypass-security></bypass-security>
+  ''',
+  directives: const [
+    BypassSecurityComponent,
+    InnerHtmlBindingComponent,
+  ],
+)
 class AppComponent {}
