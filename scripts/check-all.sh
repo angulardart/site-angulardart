226b3f9043af4af741a64e65ab38e2c04aa2ba66
Patrice Chalin <chalin@users.noreply.github.com>
2017-06-21 12:26:42 -0400

    chore(travis): only perform build checks after a build (#764)

---
+++ scripts/check-all.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+[[ -z "$NGIO_ENV_DEFS" ]] && . ./scripts/env-set.sh
+
+if [[ -n $TRAVIS && $CI_TASK != build* ]]; then
+  echo "check-all: nothing to check since this isn't a build task."
+  exit 0;
+fi
+
+travis_fold start check_build
+(set -x; ./scripts/check-build.sh)
+travis_fold end check_build
+
+travis_fold start check_links
+(set -x; ./scripts/check-links.sh)
+travis_fold end check_links
+
+travis_fold start check_code_excerpts
+(set -x; ./scripts/check-code-excerpts.sh)
+travis_fold end check_code_excerpts
