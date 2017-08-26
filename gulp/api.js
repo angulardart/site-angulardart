df0fae5ed09cc4ffbf7ab8cce9117ef3c387fc6a
Patrice Chalin <chalin@users.noreply.github.com>
2017-08-25 20:19:39 -0400

    Add "API home" button to API entry pages (#973)
    Fixes #959
    
    Staged at: https://angulardart-org-dev.firebaseapp.com/api

--- gulp/api.js
+++ gulp/api.js
@@ -21,6 +21,21 @@ module.exports = function (gulp, plugins, config) {
     return plugins.path.resolve(config.repoPath[p], config.relDartDocApiDir);
   }
 
+  // const linkToApiHome = `<a href="/api" class="webdev-api-home-button"></a>`;
+  // const homeButtonStyle = `
+  //   .webdev-api-home-button {
+  //     content: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.0' x='0px' y='0px' viewBox='0 0 24 24' style='display: inline-block; width: 24px; margin-right: 4px;'><g id='surface1'><path style=' ' d='M 20 20 L 20 12 L 22 12 L 12 3 L 2 12 L 4 12 L 4 20 C 4 20.601563 4.398438 21 5 21 L 10 21 L 10 14 L 14 14 L 14 21 L 19 21 C 19.601563 21 20 20.601563 20 20 Z '></path></g></svg>");
+  //     /*background: no-repeat url(...);*/
+  //     height: 24px;
+  //     width: 24px;
+  //     margin-right: 4px;
+  //   }
+  // `;
+  const linkToApiHome = `<a href="/api" title="Webdev API reference home">
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 24 24" style="display: inline-block; width: 24px; margin-right: 4px;"><g id="surface1"><path style=" " d="M 20 20 L 20 12 L 22 12 L 12 3 L 2 12 L 4 12 L 4 20 C 4 20.601563 4.398438 21 5 21 L 10 21 L 10 14 L 14 14 L 14 21 L 19 21 C 19.601563 21 20 20.601563 20 20 Z "></path></g></svg>
+    </a>
+  `;
+
   // Copy (and patch) API docs to ${destPath(p)} for each project p specified via --dartdoc
   gulp.task('finalize-api-docs', config.dartdocProj.map(p => `finalize-api-docs-${p}`));
 
@@ -36,6 +51,7 @@ module.exports = function (gulp, plugins, config) {
     ];
     const indexHtml = filter(`${baseDir}/index.html`, { restore: true });
     const ngContentAstIndex = filter(`${baseDir}/angular2.compiler/NgContentAst/index.html`, { restore: true });
+    //const stylesFile = filter(`${baseDir}/**/static-assets/styles.css`, { restore: true });
     return gulp.src(src, { base: baseDir })
       .pipe(indexHtml)
       .pipe(plugins.replace(/<\/title>/, `$&\n  <base href="/api/${pkgName}/">`, { skipBinary: true }))
@@ -45,6 +61,11 @@ module.exports = function (gulp, plugins, config) {
       // Patch file; see https://github.com/dart-lang/site-webdev/issues/271
       .pipe(plugins.replace(/(<base href="..)\/..(">)/, '$1$2'))
       .pipe(ngContentAstIndex.restore)
+
+      .pipe(plugins.replace(/<header id="title">/, `$&\n  ${linkToApiHome}`, { skipBinary: true }))
+      // .pipe(stylesFile)
+      // .pipe(plugins.replace(/$/, homeButtonStyle, { skipBinary: true }))
+      // .pipe(stylesFile.restore)
 
       .pipe(gulp.dest(path.join(config.unifiedApiPath, pkgName)));
   }
