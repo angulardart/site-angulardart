5d617bb55cfc8d803c780ccc40218ae2a1459f01
Patrice Chalin <chalin@users.noreply.github.com>
2017-08-31 20:28:46 -0400

    chore: ensure that diff2html is installed so that code diffs work (#994)

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -19,7 +19,7 @@ travis_fold end before_install.ruby_bundler
 ./scripts/install-dart-sdk.sh
 
 travis_fold start before_install.npm_install
-  (set -x; npm install --global firebase-tools gulp-cli superstatic --no-optional)
+  (set -x; npm install --global diff2html-cli firebase-tools gulp-cli superstatic --no-optional)
 travis_fold end before_install.npm_install
 
 travis_fold start before_install.ceu
