3e7dea0a3f8c8960cf63579ef1fb3926d220a4c2
Patrice Chalin <chalin@users.noreply.github.com>
2017-09-27 16:48:42 -0400

    chore: fix content_shell font issues (#1078)
    Fixes #1080
    
    Also further restrict Travis jobs for which content_shell gets installed.

--- scripts/browser.sh
+++ scripts/browser.sh
@@ -4,12 +4,12 @@ set -e -o pipefail
 
 [[ -z "$NGIO_ENV_DEFS" ]] && . ./scripts/env-set.sh > /dev/null
 
-if [[ $CI_TASK == build* ]]; then
-  echo "We don't launch the content_shell for CI_TASK == $CI_TASK"
-else
+if [[ $CI_TASK == e2e* || $CI_TASK == test* ]]; then
   set -x
   sh -e /etc/init.d/xvfb start
   t=0; until (xdpyinfo -display :99 &> /dev/null || test $t -gt 10); do sleep 1; let t=$t+1; done
   content_shell --run-layout-test --disable-gpu-early-init --disable-gpu
   set +x
+else
+  echo "We don't launch the content_shell for CI_TASK == $CI_TASK"
 fi
