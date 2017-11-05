1fe6987d33859c1fbe93e7dc11d8ec6d7c9d9131
Patrice Chalin <chalin@users.noreply.github.com>
2017-11-05 08:10:31 -0500

    Auto-staging round-robin over two staging servers (#1163)
    Fixes #833

---
+++ scripts/deploy/pr.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+if [[ -z $TRAVIS ]]; then
+  exit 0;
+fi
+
+AUTO_STAGING_FB_PROJ_ID="$(($TRAVIS_JOB_ID % 2))"
+
+if [[ $TRAVIS_REPO_SLUG == dart-lang** && \
+      $CI_TASK == build* && \
+      $TRAVIS_PULL_REQUEST != false ]];
+then
+  ./scripts/deploy/firebase.sh auto-staging-$AUTO_STAGING_FB_PROJ_ID
+elif [[ $TRAVIS_PULL_REQUEST == false ]]; then
+  echo "Travis event type: $TRAVIS_EVENT_TYPE"
+  echo "Not a PR. Skipping PR auto-deploy."
+fi
