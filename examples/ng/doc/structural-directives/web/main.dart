bdef38cf10a1d3e6c5da43f7eae1f038103dd108
Patrice Chalin <chalin@users.noreply.github.com>
2018-01-12 16:42:24 -0500

    cleanup of structural-directives example app (#1258)

--- examples/ng/doc/structural-directives/web/main.dart
+++ examples/ng/doc/structural-directives/web/main.dart
@@ -1,25 +1,6 @@
-import 'dart:html';
-
 import 'package:angular/angular.dart';
 import 'package:structural_directives/app_component.dart';
 
 void main() {
-  // bootstrap(StructuralDirectivesComponent);
-  bootstrap(AppComponent,
-      // https://github.com/dart-lang/angular/issues/277
-      [provide(ExceptionHandler, useClass: BrowserExceptionHandler)]);
-}
-
-@Injectable()
-class BrowserExceptionHandler implements ExceptionHandler {
-  const BrowserExceptionHandler();
-
-  @override
-  void call(exception, [stackTrace, String reason]) {
-    window.console.error(ExceptionHandler.exceptionToString(
-      exception,
-      stackTrace,
-      reason,
-    ));
-  }
+  bootstrap(AppComponent);
 }
