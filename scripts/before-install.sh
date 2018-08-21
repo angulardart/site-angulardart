c0ad9f2744b31fe852938fe543a0a47c0633fb02
Patrice Chalin <chalin@users.noreply.github.com>
2018-08-21 18:04:42 -0400

    Reinstate link checking, and refresh submodules (#1792)
    Site-webdev counterpart to the site-www fix of https://github.com/dart-lang/site-www/issues/1051.
    
    Related: https://github.com/dart-lang/site-www/issues/1091

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -4,6 +4,16 @@ set -e -o pipefail
 
 [[ -z "$DART_SITE_ENV_DEFS" ]] && . ./scripts/env-set.sh
 
+
+if [[ "$1" == --force ]]; then FORCE=1; shift; fi
+
+if [[ -n "$TRAVIS" || -n "$FORCE" || "$1" == --up* ]]; then
+  PUB_CMD="upgrade"
+  if [[ -n "$1" ]]; then shift; fi
+else
+  PUB_CMD="get"
+fi
+
 if [[ -n "$TRAVIS" ]]; then
   ./scripts/env-info-and-check.sh
   # travis_fold start before_install.update_apt_get
@@ -11,33 +21,65 @@ if [[ -n "$TRAVIS" ]]; then
   # travis_fold end before_install.update_apt_get
 fi
 
+function pub_global_activate() {
+  PKG="$1"
+  if [[ -n "$TRAVIS" || -n "$FORCE" || ! $(pub global run $PKG -h) ]]; then
+    echo "INFO: activating $PKG"
+    (set -x; pub global activate $PKG)
+  else
+    echo "INFO: pub global activate $PKG, already activated"
+    echo "INFO: package already activated: $PKG (pub global activate $PKG)"
+  fi
+}
+
+function npm_global_install() {
+  PKG="$1"
+  CMD=${PKG/@*/}
+  CMD=${CMD/-cli/}
+  CMD=${CMD/-tools/}
+  if [[ -n "$TRAVIS" || -n "$FORCE" || -z "$(type -t $CMD)" ]]; then
+    echo "INFO: installing $PKG ($CMD)"
+    (set -x; npm install --global $PKG --no-optional)
+  else
+    echo "INFO: package already installed: $PKG ($CMD)"
+  fi
+}
+
 travis_fold start before_install.npm_install_shared
-  (set -x; npm install --global gulp-cli --no-optional)
+  npm_global_install gulp-cli
 travis_fold end before_install.npm_install_shared
 
 travis_fold start before_install.webdev
-  (set -x; pub global activate webdev)
+  pub_global_activate webdev
 travis_fold end before_install.webdev
 
 ./scripts/install-dart-sdk.sh
 
 if [[ -z "$CI_TASK" || "$CI_TASK" == build* ]]; then
   # Jekyll needs Ruby and the Ruby bundler
+  PKG="bundler"
   travis_fold start before_install.ruby_bundler
-    (set -x; gem install bundler)
+    if [[ -n "$TRAVIS" || -n "$FORCE" || -z "$(type -t $PKG)" ]]; then
+      (set -x; gem install $PKG)
+    else
+      echo "INFO: package already installed: $PKG"
+    fi
   travis_fold end before_install.ruby_bundler
 
   travis_fold start before_install.npm_install_shared
-    (set -x; npm install --global diff2html-cli firebase-tools superstatic --no-optional)
+      npm_global_install diff2html-cli
+      npm_global_install firebase-tools@4.0.3
+      npm_global_install superstatic@5.0.2
   travis_fold end before_install.npm_install_shared
 
   travis_fold start before_install.dartdoc
-    (set -x; pub global activate dartdoc)
+    pub_global_activate dartdoc
   travis_fold end before_install.dartdoc
 
   ./scripts/get-ng-repo.sh
 fi
 
 travis_fold start before_install.pub
-pub upgrade
+  echo "pub $PUB_CMD"
+  pub $PUB_CMD
 travis_fold end before_install.pub
--- scripts/check-after-site-build.sh
+++ scripts/check-after-site-build.sh
@@ -10,9 +10,7 @@ if [[ -n $TRAVIS && $CI_TASK != build* ]]; then
 fi
 
 travis_fold start check_links
-# (set -x; ./scripts/shared/check-links.sh)
-# FIXME(https://github.com/dart-lang/site-www/issues/1051): reinstate linkchecking
-echo "WARNING: skipping linkcheck due to https://github.com/dart-lang/site-www/issues/1051"
+(set -x; ./scripts/shared/check-links.sh)
 travis_fold end check_links
 
 # Check output from Jekyll plugin
