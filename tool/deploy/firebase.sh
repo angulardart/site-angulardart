91d0c1c7f30e21f982cbe8319e72bb0c528a6cbe
Patrice Chalin <chalin@users.noreply.github.com>
2018-09-15 08:34:44 -0400

    chore: quickfix for deploy script
    There are currently multiple deploy scripts. Those are being consolidated into one single script. We'll also be addressing robots.txt issue. In the meantime, here is a quick fix so that we can get deploys working again.

--- tool/deploy/firebase.sh
+++ tool/deploy/firebase.sh
@@ -31,4 +31,4 @@ FB_PROJ=$1
 : ${FB_PROJ:=default}
 
 echo "================ Deploy to Firebase ($FB_PROJ) ========================"
-firebase deploy --non-interactive --token "$FIREBASE_TOKEN" --project $FB_PROJ
+npx firebase deploy --non-interactive --token "$FIREBASE_TOKEN" --project $FB_PROJ
