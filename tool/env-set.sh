11190fbfd16f1279a3b484b72caa8a86dca7c360
Patrice Chalin <chalin@users.noreply.github.com>
2019-11-01 10:44:15 -0400

    Refresh site-shared, use ruby and node default vers (#118)

--- tool/env-set.sh
+++ tool/env-set.sh
@@ -1,7 +1,8 @@
 # This bash file is meant to be source'd, not executed.
 
-_DART_SITE_RUBY_VERS=ruby-2.6.5
-_DART_SITE_NODE_VERS=12
+# Set to override site-shared defaults, or unset to use default:
+unset _DART_SITE_NODE_VERS
+unset _DART_SITE_RUBY_VERS
 
 source ./tool/shared/env-set.sh
 
